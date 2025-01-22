e8f1d42b9787fc02cb3ce3e3a62efdc9cf9ce359
ankitanjana23 <ankitanjana@gmail.com>
2025-01-22 23:34:57 +0530

    final commit

--- mysqlConnect/index.js
+++ mysqlConnect/index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express();
 const PORT = 3000;
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+      host: 'localhost' ,
+      user: 'root' ,
+      password: 'Ankit@#4565' ,
+      database: 'my_db',
+})
+
+connection.connect((err) => {
+    if(!err) console.log('database connected successfully');
+    else console.log('database connection error ',err);
+})
 
 app.use(express.json())
 
+//connect backend with sql 
+//install npm i mysql
+
 app.get('/' , (req,res)=>{
     res.send({'res' : 'backend connect successfully'})
+})
+
+app.get('/api' , (req,res)=>{
+    connection.query('SELECT * FROM Marks' , (err,result) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Database query error', details: err.message });
+            return;
+          }
+          res.json(result);
+    })
 })
 
 app.listen(PORT, () => {
--- postgresqlConnect/index.js
+++ postgresqlConnect/index.js
@@ -1,14 +1,35 @@
 const express = require("express")
 const app = express()
 
+
+//connect postgresql 
+const {Pool} = require('pg')
+const connection = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    password: 'Ankit@#4565',
+    database: 'my_db'
+  });
+
+
 const PORT = 3001;
 
 app.use(express.json())
 
+app.get('/api', async (req, res) => {
+    try {
+      const result = await connection.query('SELECT * FROM Marks');
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
 app.get('/' , (req,res) =>{
-    res.send('data')
+    res.status(200).send('Backend Connected Successfully')
 })
 
 app.listen(PORT , () => {
-    console.log(`Server connected Port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
