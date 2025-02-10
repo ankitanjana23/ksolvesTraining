c9c7ddcd0cfa753753aba39e8ad2ba1f6fdf476f
ankitanjana23 <ankitanjana@gmail.com>
2025-02-10 11:41:18 +0530

    add stream file

--- fileSystem/stream/index.js
+++ fileSystem/stream/index.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
-// const fs = require("fs");
-// const stream = require("stream");
-// const status = require("express-status-monitor");
-
-// app.use(status);
-
-/* first approch fs 
 app.get("/", (req, res) => {
-  fs.readFile("simple.txt", "utf-8", (data, err) => {
-    if (err) throw err;
-    res.send(data);
+  const stream = fs.createReadStream("simple.txt", "utf-8");
+  stream.pipe(res); // Efficiently pipes the file to response
+
+  stream.on("error", (err) => {
+    console.error(err);
+    res.status(500).send("Error reading the file");
   });
-});
-*/
 
-console.log("hii");
-app.get("/", (req, res) => {
-  console.log("hii");
-  res.send("hellow");
+  /*
+
+  stream.on("data", (chunk) => {
+    console.log(`Received chunk: ${chunk}`);
+    res.send(chunk);
+  });
+  stream.on("end", () => {
+    console.log("display data successfully");
+  });
+  stream.on("error", (err) => {
+    console.error(err);
+  });
+  */
 });
 
 app.listen(3000, () => {
